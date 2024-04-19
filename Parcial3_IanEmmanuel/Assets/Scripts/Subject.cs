42a833a5a404a4cd41b7d3692a6dd470eaace414
Ian Reyes <156475427+ianreyess@users.noreply.github.com>
2024-04-19 12:28:05 -0600

    Subject.cs

--- Parcial3_IanEmmanuel/Assets/Scripts/Subject.cs
+++ Parcial3_IanEmmanuel/Assets/Scripts/Subject.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-/// <summary>
-/// Este es nuestro ScriptableObject: Nos va a permitir rellenar en el inspector nuestras preguntas, respuestas y designar cual es la respuesta correcta
-/// Lo anterior en forma de lista
-/// </summary>
 [CreateAssetMenu(fileName = "Name Subject", menuName = "ScriptableObjects/New_Lesson", order = 1)]
 public class Subject : ScriptableObject
 {
-    /// <summary>
-    /// Lesson: aquí guardaremos en el Inspector el numero de la leccion
-    /// leccionList: Nos va a servir para generar una lista idividual por cada leccion
-    /// </summary>
     [Header("GameObject Configuration")]
     public int Lesson = 0;
 
