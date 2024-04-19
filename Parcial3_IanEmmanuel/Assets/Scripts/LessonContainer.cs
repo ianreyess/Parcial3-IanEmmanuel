7a1db20dab4ea3156ba995c6d4e71efbf2d0069a
Ian Reyes <156475427+ianreyess@users.noreply.github.com>
2024-04-19 11:54:33 -0600

    LessonContainer.cs

--- Parcial3_IanEmmanuel/Assets/Scripts/LessonContainer.cs
+++ Parcial3_IanEmmanuel/Assets/Scripts/LessonContainer.cs
@@ -5,28 +5,24 @@ using UnityEngine.UI;
 using TMPro;
 
 /// <summary>
-/// Esta clase esta encargada de gestionar las lecciones: agarra el contenido de las preguntas y respuestas anteriormente guardadas
-/// y se encarda de lo que va a aparecer en la UI
+/// La clase gestiona las lecciones, se encarga de lo que aparece en la ui con as preguntas y respuestas ya guardadas. 
 /// </summary>
 public class LessonContainer : MonoBehaviour
 {
-    // Variables para configurar la lección:
-    //Header= nos ayuda a clasificar y separar nuestras variables en el inspector por medio de etiquetas 
+    //Variable que configura la lecciÃ³n
+    //Header= clasifica y separa las variables en el inspector.
     [Header("GameObject Configuration")]
-    //aquí vamos a personalizar desde el inspector toda nuestra informacion que va a aparecer en el UI
     public int lection = 0;
     public int currentLession = 0;
     public int totalLession = 0;
     public bool areAllLessonsComplete = false;
-
-    // Variables para configurar la interfaz de usuario
+    //Configura la interfaz de usuario
     [Header("UI Configuration")]
-    //estas variables TMP_Text ya están creadas en el canva, esas se enlazarán con estas
     public TMP_Text stageTitle;
     public TMP_Text lessonStage;
 
     [Header("External GameObject Configuration")]
-    //esta es nuestra imagen principal que contiene las UI de los textos y el boton
+    //Configuracion de la imagen principal 
     public GameObject lessonContainer;
 
     [Header("Lesson Data")]
@@ -34,13 +30,9 @@ public class LessonContainer : MonoBehaviour
     public string LessonName;
 
 
-    /// <summary>
-    /// Verifica si enlazamos el gameObject o nuestra imagen de lessonContainer, donde nos da la informacion de que leccion es,
-    /// si no está enlazado, en la consola nos avisará que está vacío el contenedor con un mensaje de advertencia
-    /// </summary>
     void Start()
     {
-        //este va a verificar si nuestra imagen UI principal con toda nuestra infomacion está ligada en el inspector
+        //Verifica que la imagen sÃ­ estÃ© ligado al inspector.
         if(lessonContainer != null)
         {
             OnUpdateUI();
@@ -51,22 +43,12 @@ public class LessonContainer : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Este metodo verifica si están enlazados a nuestro contenedor de la variable los textos, 
-    /// <c>
-    /// 1. Que nos comunica la Leccion principal y actual seleccionada 
-    /// 2. En cual de los niveles nos encontramos y si llevamos algun avance
-    /// estos se ligan a otras variables que le informa que botón fue seleccionado para saber que imprimir en la UI
-    /// </c>
-    /// 
-    /// En caso contrario, tenemos un else que nos avisa que tenemos le ligar nuestros textos, ya que actualmente se encuentran vacíos
-    /// </summary>
+    
     public void OnUpdateUI()
     {
-        //este va a verificar que nuestros textos estan ligados a nuestro inspector en sus variables correspondientes
+        //Verifica que el texto estÃ© ligado al inspector.
         if(stageTitle != null || lessonContainer != null) 
         {
-            //Los .text solo funcionan con variables TMP_Stage
             stageTitle.text = "Leccion " + lection;
             lessonStage.text = "Leccion " + currentLession + " de " + totalLession;
         }
@@ -76,24 +58,17 @@ public class LessonContainer : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// A la hora de jugar, este se encarga de aparecer y desparecer nuestra ventana de lecciones
-    /// detecta si se interactuo con el boton, al hacer clic: se activa o se desactiva la ventana
-    /// </summary>
-    //Esta es una funcion o metodo que nosotros creamos para personalizar por nuestra cuenta un evento
-    //este metodo especificamente activa/desactiva la ventana de lessonContainer
     public void EnableWindow()
     {
         OnUpdateUI();
-                            //activeSelf es si está activado
+                            //activeSelf verifica que estÃ© activo
         if(lessonContainer.activeSelf)
         {
-            //desactiva el objeto si está activo
+            //Lo desacctiva.
             lessonContainer.SetActive(false);
         }
         else
         {
-            //activa el objeto si está desactivado
             lessonContainer.SetActive(true);
             MainScript.instance.SetSelectedLesson(LessonName);
         }
